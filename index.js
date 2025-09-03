7fae0f77334fe95ec7e5ac774aa260a0cf4e81ad
bradley <fazebrad13@gmail.com>
2025-09-03 12:57:52 +0300

    delteing html

---
+++ index.js
@@ -0,0 +1,25 @@
+const studentProfile = {
+  name: "Aisha",
+  age: 21,
+  courses: [
+    {
+      title: "JavaScript Basics",
+      completed: true,
+      scores: [85, 90, 88]
+    },
+    {
+      title: "Web Development",
+      completed: false,
+      scores: [78, 82, 80]
+    }
+  ],
+  contact: {
+    email: "aisha@example.com",
+    social: {
+      twitter: "@aishacodes",
+      github: "aisha-dev"
+    }
+  }
+};
+
+console.log('')
